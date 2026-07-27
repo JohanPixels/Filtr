b67b07fe046fc9674874e1a3d0c07a62e9ad6db4
JohanPixels <johanmunoz319@gmail.com>
2026-07-26 20:45:41 -0500

    feat: configurar CORS para frontend Svelte

---
+++ backend/app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from routes import contratistas, contratos, entidades
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(contratos.router)
+app.include_router(contratistas.router)
+app.include_router(entidades.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hola Filtr :D"}
